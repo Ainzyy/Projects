010e0ac77945b809a794999ef207c6e1e80c3aaa
Ainz <108608846+Ainzyy@users.noreply.github.com>
2024-03-21 13:21:16 +0800

    BaseModal: With Error

--- Weather App/tailwind.config.js
+++ Weather App/tailwind.config.js
@@ -2,10 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-    colors: {
-      "weather-primary": "#00668A",
-      "weather-secondary": "#004E71",
+    extend: {
+      colors: {
+        "weather-primary": "#00668A",
+        "weather-secondary": "#004E71",
+      },
     },
   },
   fontFamily: {
